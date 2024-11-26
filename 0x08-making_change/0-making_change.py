96d588415912987c0a2949f9fa84a481a32ab9ff
Almontheir <141272895+Almontheir@users.noreply.github.com>
2024-11-26 10:36:44 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,25 @@
-interface MajorCredits {
-  credits: number;
-  brand: "MajorCredits";
-}
+#!/usr/bin/python3
+"""Change making module.
+"""
 
-interface MinorCredits {
-  credits: number;
-  brand: "MinorCredits";
-}
 
-function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  return {
-    credits: subject1.credits + subject2.credits,
-    brand: "MajorCredits",
-  };
-}
-
-function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  return {
-    credits: subject1.credits + subject2.credits,
-    brand: "MinorCredits",
-  };
-}
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
